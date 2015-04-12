aa755b69810fe35bf615e911d1a743193de99ceb
Jamen Marz <jamen.marz@gmail.com>
2015-04-12 17:14:52 -0600

    Added lib object.

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var fs = require("fs");
+var lib = require("./lib");
+
+// Chat
+app.get("/", function(req, res){
+  res.writeHead(200, {"Content-Type": "text/html"});
+  fs.readFileSync(__dirname + "/pages/chat.html", function(err, data){
+    if (err) throw err;
+    res.send(data);
+  });
+});
+
+app.get("/static/*", function(req, res){
+  lib.static(req, res, fs)
+});
+
+io.on("connection", function(socket){
+  lib.chat(socket);
+});
+
+http.listen(80, function(){
+  console.log(" - Chat online.");
+});
